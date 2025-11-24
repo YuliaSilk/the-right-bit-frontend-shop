b29e1fc719a7bcc1e466811c3c5c0bf2ab6708fa
YuliaSilk <yuliashevchyk0506@gmail.com>
2025-11-24 21:54:14 +0200

    change search logic

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -3,17 +3,22 @@ import logo from "@/assets/images/logo.svg";
 import searchIcon from "@/assets/icons/search.png";
 import userIcon from "@/assets/icons/profile.png";
 import cartIcon from "@/assets/icons/cart.png";
+import SearchDropdown from "../../home/SearchComponent/SearchDropdown";
 
 import {Link} from "react-router-dom";
 import {useAuth} from "@/context/AuthContext";
 import {useSearch} from "../../../context/SearchContext";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 
 const Header = () => {
  const {isAuthenticated} = useAuth();
  const {searchTerm, setSearchTerm} = useSearch();
  const [menuOpen, setMenuOpen] = useState(false);
+ const [filtered, setFiltered] = useState([]);
+ const [allProducts, setAllProducts] = useState([]);
+ const [showDropdown, setShowDropdown] = useState(false);
+
  const navigate = useNavigate();
 
  const handleSearch = (e) => {
@@ -21,6 +26,26 @@ const Header = () => {
   if (!searchTerm.trim()) return;
   navigate(`/catalog?search=${encodeURIComponent(searchTerm)}`);
  };
+ useEffect(() => {
+  fetch(import.meta.env.VITE_API_URL + "/api/v1/catalog?size=1000")
+   .then((r) => r.json())
+   .then((data) => {
+    const items = Array.isArray(data) ? data : data.content || [];
+    setAllProducts(items);
+   });
+ }, []);
+
+ useEffect(() => {
+  const term = searchTerm.toLowerCase();
+  if (!term) {
+   setFiltered([]);
+   return;
+  }
+
+  const res = allProducts.filter((p) => p.productName?.toLowerCase().includes(term));
+
+  setFiltered(res);
+ }, [searchTerm, allProducts]);
 
  return (
   <header className={styles.header}>
@@ -77,6 +102,8 @@ const Header = () => {
       <form
        className={styles.searchBox}
        onSubmit={handleSearch}
+       onFocus={() => setShowDropdown(true)}
+       onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
       >
        <input
         type="text"
@@ -85,6 +112,7 @@ const Header = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
         className={styles.searchInput}
        />
+
        <button
         type="submit"
         className={styles.searchButton}
@@ -94,6 +122,15 @@ const Header = () => {
          alt="Search"
         />
        </button>
+       {showDropdown && (
+        <div className={styles.dropdownWrapper}>
+         <SearchDropdown
+          searchTerm={searchTerm}
+          results={filtered}
+          onSelect={setSearchTerm}
+         />
+        </div>
+       )}
       </form>
 
       <div className={styles.icons}>
